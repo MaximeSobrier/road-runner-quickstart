36e2244c6cc84ad3d3e8ba00b51fcdff2fbbe8a3
vik-j <114836259+vik-j@users.noreply.github.com>
2024-11-17 16:28:34 -0800

    Updated blue bucket side
    - Added the 3 sample(Not completed)
    -Changed to moving robot to pick up samples
    -Saved time on autons
    -Could not fix risk flicking problem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueBasketSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueBasketSide.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.auton;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
@@ -31,14 +32,18 @@ public class BlueBasketSide extends LinearOpMode {
                 .waitSeconds(4.8)
                 //Arm to high speci and back down
                 .strafeToLinearHeading(new Vector2d(-49,-43), Math.toRadians(90))
-                .waitSeconds(2)
-                .strafeToLinearHeading(new Vector2d(-48,-46), Math.toRadians(45))
-                .waitSeconds(6.2)
+                .waitSeconds(0.5)
+                .strafeToLinearHeading(new Vector2d(-49,-38), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(-48,-44.5), Math.toRadians(45))
+                .waitSeconds(5.5)
                 //intake
                 .strafeToLinearHeading(new Vector2d(-60,-45), Math.toRadians(90))
-                .waitSeconds(2.75)
+                .waitSeconds(0.5)
+                .strafeToLinearHeading(new Vector2d(-60,-38), Math.toRadians(90))
                 .strafeToLinearHeading(new Vector2d(-49.5,-43.5), Math.toRadians(45))
-                .waitSeconds(6);
+                .waitSeconds(5.3)
+                .strafeToLinearHeading(new Vector2d(-60,-38), Math.toRadians(135))
+                .strafeToLinearHeading(new Vector2d(-49.5,-43.5), Math.toRadians(45));
 
 
         // Final action to close out the trajectory
@@ -48,7 +53,7 @@ public class BlueBasketSide extends LinearOpMode {
                 .afterTime(0.01, bot.setPidVals(1050,3800))
 //                .afterTime(0.05, bot.intake(-0.5))
                 .afterTime(0.02, telemetryPacket -> {
-                    bot.wrist.setPosition(0.07);
+                    bot.wrist.setPosition(0.01);
                     return false;
                 })
                 .afterTime(0.8, telemetryPacket -> {
@@ -57,20 +62,20 @@ public class BlueBasketSide extends LinearOpMode {
                     return false;
                 })
                 .afterTime(2.3, telemetryPacket -> {
-                    bot.wrist.setPosition(0.07);
+                    bot.wrist.setPosition(0.01);
                     return false;
                 })
                 .afterTime(2.8, telemetryPacket -> {
-                    bot.wrist.setPosition(0.07);
+                    bot.wrist.setPosition(0.01);
                     return false;
                 })
-                .afterTime(2.3, bot.setPidVals(700,3800))
+                .afterTime(2.3, bot.setPidVals(600,3950))
                 .afterTime(3.2, telemetryPacket -> {
-                    bot.wrist.setPosition(0.07);
+                    bot.wrist.setPosition(0.01);
                     return false;
                 })
                 .afterTime(3.7, telemetryPacket -> {
-                    bot.wrist.setPosition(0.07);
+                    bot.wrist.setPosition(0.01);
                     return false;
                 })
                 .afterTime(3.8, telemetryPacket -> {
@@ -95,7 +100,6 @@ public class BlueBasketSide extends LinearOpMode {
                     bot.intakeRight.setPower(-1);
                     return false;
                 })
-                .afterTime(8.5, bot.setPidVals(0, 1800))
                 .afterTime(9.3, telemetryPacket -> {
                     bot.intakeLeft.setPower(0.3);
                     bot.intakeRight.setPower(-0.3);
@@ -108,18 +112,17 @@ public class BlueBasketSide extends LinearOpMode {
                 })
                 .afterTime(11.51,bot.setPidVals(2100,0))
                 .afterTime(12.9, bot.setPidVals(2100,6000))
-                .afterTime(14.6, telemetryPacket -> {
+                .afterTime(15.2, telemetryPacket -> {
                     bot.intakeLeft.setPower(-0.4);
                     bot.intakeRight.setPower(0.4);
                     return false;
                 })
-                .afterTime(15.1, telemetryPacket -> {
+                .afterTime(15.3, telemetryPacket -> {
                     bot.wrist.setPosition(0.07);
                     return false;
                 })
-                .afterTime(15.3, bot.setPidVals(2100,0))
+                .afterTime(15.5, bot.setPidVals(2100,0))
                 .afterTime(16.1, bot.setPidVals(0,0))
-                .afterTime(17.8, bot.setPidVals(0, 1800))
                 .afterTime(16.5, telemetryPacket -> {
                     bot.intakeLeft.setPower(1);
                     bot.intakeRight.setPower(-1);
@@ -142,6 +145,24 @@ public class BlueBasketSide extends LinearOpMode {
                     bot.intakeRight.setPower(0.4);
                     return false;
                 })
+                .afterTime(25.5, bot.setPidVals(2100,0))
+                .afterTime(25.6, telemetryPacket -> {
+                    bot.wrist.setPosition(0.07);
+                    return false;
+                })
+                .afterTime(26, telemetryPacket -> {
+                    bot.intakeLeft.setPower(1);
+                    bot.intakeRight.setPower(-1);
+                    return false;
+                })
+                .afterTime(27, bot.setPidVals(0,0))
+                .afterTime(28.5, bot.setPidVals(2100,0))
+                .afterTime(29.5, telemetryPacket -> {
+                    bot.intakeLeft.setPower(-0.4);
+                    bot.intakeRight.setPower(0.4);
+                    return false;
+                })
+                .afterTime(30, bot.setPidVals(0,0))
 
                 .build();
 
