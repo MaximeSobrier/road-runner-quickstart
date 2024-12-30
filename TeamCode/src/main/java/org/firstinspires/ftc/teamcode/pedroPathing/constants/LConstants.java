bc0f8feec401e08f6de4263856fc036dfa89b041
vik-j <114836259+vik-j@users.noreply.github.com>
2024-12-30 14:56:58 -0800

    pedro library

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.constants;
+
+import com.pedropathing.localization.*;
+import com.pedropathing.localization.constants.*;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+public class LConstants {
+    static {
+        TwoWheelConstants.forwardTicksToInches = .001989436789;
+        TwoWheelConstants.strafeTicksToInches = .001989436789;
+        TwoWheelConstants.forwardY = 6.2742459229;
+        TwoWheelConstants.strafeX = -1.643933717;
+        TwoWheelConstants.forwardEncoder_HardwareMapName = "leftBack";
+        TwoWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
+        TwoWheelConstants.forwardEncoderDirection = Encoder.FORWARD;
+        TwoWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+        TwoWheelConstants.IMU_HardwareMapName = "imu";
+        TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+    }
+}
+
+
+
+
